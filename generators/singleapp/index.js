535217c6141b2958002c02a1d67b311c2edc29d7
lkot <constantin3627@gmail.com>
2015-02-26 22:39:51 +0800

    BG-4: create function generateFileStruct; create function format string; write file struct json

--- generators/singleapp/index.js
+++ generators/singleapp/index.js
@@ -15,7 +15,54 @@ module.exports = yeoman.generators.Base.extend({
 
   },
 
-  writing: function () {
-    this.mkdir(this.appname);
+  writing: {
+    appDirectories: function () {
+      var structJSON = this.src.readJSON('../struct.json');
+
+      var self = this;
+
+      generateFileStruct(structJSON);
+
+      //////////////////////////////
+
+      /**
+       * Recursion function for creating directory structure by
+       * structure map json.
+       *
+       * @param sj {Object}
+       * @param path {String}
+       */
+      function generateFileStruct(sj, path) {
+        path = path || './';
+
+        var dirName = path + _(sj.name, {appName: self.appname});
+
+        self.dest.mkdir(dirName);
+
+        if (sj.dirs) {
+          self._.each(sj.dirs, function(d) {
+            generateFileStruct(d, dirName + '/');
+          });
+        }
+      }
+
+      /**
+       * Get formating string and params and return result string.
+       *
+       * Usage:
+       *
+       * _('I'm {age} years old', {age: 10})
+       *
+       * @param tpl {String}
+       * @param opts {Object}
+       * @returns {String}
+       * @private
+       */
+      function _(tpl, opts) {
+        self._.each(self._.keys(opts), function (opt){
+          tpl = tpl.replace('{'+opt+'}', opts[opt]);
+        }); return tpl;
+      }
+    }
   }
 });
