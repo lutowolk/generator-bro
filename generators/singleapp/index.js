c608e05eb547b81c6aadc68aede42da691f4a69c
lkot <constantin3627@gmail.com>
2015-03-01 14:05:35 +0800

    BG-7: integration with python script for singleapp generator; update existing fyles: installed_apps and root urls conf

--- generators/singleapp/index.js
+++ generators/singleapp/index.js
@@ -25,7 +25,32 @@ module.exports = yeoman.generators.Base.extend({
     appDirectories: function () {
       var structJSON = this.fs.readJSON(
         this.templatePath('../struct.json'));
+
       generateFileStruct(structJSON, './', this);
     }
+  },
+
+  updateExistingFiles: function () {
+    var scriptPath = this.config.get('path') + '/scripts/update_file.py';
+
+    if(!this.fs.exists(scriptPath)) {
+      this.log('Error. Path to the python script `update_file.py` is not found');
+      return false;
+    }
+
+    this.spawnCommand('python',
+      [scriptPath,
+        '-c', 'add_url_to_patterns',
+        '-p', './server/config/urls.py',
+        '-t', 'urlpatterns',
+        '-r', '^' + this.appname + '/',
+        '-u', 'apps.'+ this.appname +'.urls']);
+
+    this.spawnCommand('python',
+      [scriptPath,
+        '-c', 'add_str_to_tuple',
+        '-p', './server/config/settings/installed_apps.py',
+        '-t', 'LOCAL_APPS',
+        '-v', 'apps.' + this.appname]);
   }
 });
