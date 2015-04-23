aea39c7e348eea93cdaee2df0bc80c51583b1a34
lkot <constantin3627@gmail.com>
2015-04-23 23:09:10 +0800

    BG-18: removed logic for generating fuzzy model

--- lib/generators/sub/templates/_urls.py
+++ lib/generators/sub/templates/_urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, include, url
-from apps.{{appName}}.views import {{capitalize appName}}ListView, {{capitalize appName}}DetailView
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', {{capitalize appName}}ListView.as_view(), name='{{appName}}.list'),
-    url(r'^(?P<slug>[-_\w]+)/$', {{capitalize appName}}DetailView.as_view(), name='{{appName}}.detail'),
 )
