f22aba250737369cbe9ae39f3d8abd7b64364606
lkot <constantin3627@gmail.com>
2015-05-04 16:04:20 +0800

    BG-21: add options model for set custom file with model and view for set custom file view; create test case for set model options

--- lib/generators/view/templates/_form.py
+++ lib/generators/view/templates/_form.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.forms import ModelForm
-from apps.{{lower appName}}.models import {{modelName}}
+from {{modelPythonPath}} import {{modelName}}
 
 
 class {{modelName}}Form(ModelForm):
