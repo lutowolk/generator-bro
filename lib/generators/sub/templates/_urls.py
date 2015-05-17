7ebf03904d51d3cf460e3c4d17739e8f99fceda5
lkot <constantin3627@gmail.com>
2015-05-17 17:57:46 +0800

    BG-34: add support drf for sub generator

--- lib/generators/sub/templates/_urls.py
+++ lib/generators/sub/templates/_urls.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+{{#if drf}}
+from {{apps}}.{{app}}.router import router
+{{/if}}
 
 
 urlpatterns = patterns(
     '',
+    {{#if drf}}
+    url(r'^', include(router.urls)),
+    {{/if}}
 )
