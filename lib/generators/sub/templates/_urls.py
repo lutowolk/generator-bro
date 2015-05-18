93fb67c3389bfa1bdcadbc6c7fc763055045ccbc
lkot <constantin3627@gmail.com>
2015-05-18 21:51:24 +0800

    BG-34: remove template router; remove router from create section; update url template

--- lib/generators/sub/templates/_urls.py
+++ lib/generators/sub/templates/_urls.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 {{#if drf}}
-from {{apps}}.{{app}}.router import router
+from rest_framework import routers
 {{/if}}
 
 
 urlpatterns = patterns(
     '',
-    {{#if drf}}
-    url(r'^', include(router.urls)),
-    {{/if}}
 )
+{{#if drf}}
+################################################################
+# api urls
+################################################################
+
+router = routers.DefaultRouter()
+
+urlpatterns += patterns('', url(r'^api/', include(router.urls)))
+{{/if}}
--- lib/generators/sub/templates/router.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from rest_framework import routers
-
-__all__ = ['router']
-
-router = routers.DefaultRouter()
