e99ac4c3a483bfb9ee3ca6e7adb602103f0f8905
lkot <constantin3627@gmail.com>
2015-06-08 12:58:15 +0800

    BG-29: add to core functionality new template tag - form tag; add template; update test case; update struct.json

--- lib/generators/app/templates/server/core.py
+++ lib/generators/app/templates/server/core.py
@@ -24,3 +24,22 @@ def list_tag(model_path=None, limit=None, order=None,
         model_items = model_items[:limit]
 
     return render_to_string(template_name, {'items': model_items})
+
+
+@register.simple_tag
+def form_tag(form_path=None, url=None, template_name='form_tag.html'):
+
+    form_path_split = form_path.split('.')
+
+    class_name = form_path_split.pop()
+    module = importlib.import_module('.'.join(form_path_split))
+
+    FormClass = getattr(module, class_name)
+
+    if url is None:
+        namespace = first(form_path_split)
+        model_name = FormClass.Meta.model.__name__
+        url = '{namespace}:{model}-list'.format(
+            namespace=namespace, model=model_name.lower())
+
+    return render_to_string(template_name, {'form': FormClass(), 'url': url})
